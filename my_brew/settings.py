2d5721ae532bdfc74b56d0414e7ba0108c4f7f49
Jordan Kubista <jkube29@gmail.com>
2021-05-11 12:44:30 -0500

    add conditional to use postgres db in production

--- my_brew/settings.py
+++ my_brew/settings.py
@@ -86,8 +86,17 @@ if DEBUG:
             'NAME': BASE_DIR / 'db.sqlite3',
         }
     }
-# else:
-
+else:
+    DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'mybrewdb',
+        'USER': 'kube',
+        'PASSWORD': 'quiksilver',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
