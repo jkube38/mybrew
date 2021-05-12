dff2e06c4f6712ebed3554e96c85ed5dacfa3de3
Jordan Kubista <jkube29@gmail.com>
2021-05-11 20:40:01 -0500

    changed media root

--- my_brew/settings.py
+++ my_brew/settings.py
@@ -143,7 +143,7 @@ STATICFILES_DIRS = [
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/images')
 
 AUTH_USER_MODEL = 'my_brew_app.MyBrewUser'
 
