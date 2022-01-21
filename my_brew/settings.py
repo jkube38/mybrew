b9f4dd928fd1251b12d0f93a86e2092f852eb717
Jordan Kubista <jkube29@gmail.com>
2022-01-20 21:58:39 -0600

    change static back

--- my_brew/settings.py
+++ my_brew/settings.py
@@ -145,7 +145,7 @@ MEDIA_URL = '/media/'
 # before pushing for deployed upgrade uncomment static root and
 # in STATICFILES_DIRS change static to staticfiles
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "staticfiles"),
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
