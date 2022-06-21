9ea12674fe3a0309fe18962dfb12e1eef542c3b8
Jordan Kubista <jkube29@gmail.com>
2022-06-21 15:15:41 -0500

    static files

--- my_brew/settings.py
+++ my_brew/settings.py
@@ -150,10 +150,10 @@ MEDIA_URL = '/media/'
 # ]
 # ^^^comment out for push and uncomment belowvvvvvv
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'staticfiles')
-]
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'staticfiles')
+# ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 
