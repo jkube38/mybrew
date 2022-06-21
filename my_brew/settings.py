f1b59d7dee395691cd8342da544595302e993e03
Jordan Kubista <jkube29@gmail.com>
2022-06-21 15:05:40 -0500

    commented out static root

--- my_brew/settings.py
+++ my_brew/settings.py
@@ -153,7 +153,7 @@ MEDIA_URL = '/media/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'staticfiles')
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 
